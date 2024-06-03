ac8fa59cda497f96da19848a6e964bf42ae84b1a
Bao Tran <tran.bao.n112@gmail.com>
2024-06-03 14:15:57 -0700

    bt - finished code and most tests

--- src/Listings.sol
+++ src/Listings.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-// import {Users} from "../src/Users.sol";
-import { Wallet } from "../src/Wallet.sol";
+// import { Users } from "../src/Users.sol";
+// import { Wallet } from "../src/Wallet.sol";
 
 contract Listings {
     // Standard listing (stocked, single price)
@@ -11,7 +11,7 @@ contract Listings {
         uint256 price;
         address seller;
         string name;
-        uint40 stockRemaining;
+        uint40 stockRemaining; // stockRemaining == 0 for inactive
 
         // if is not shippable, location is where it is sold from. if is, where shipping from
         // can be 'N/A'
@@ -24,35 +24,33 @@ contract Listings {
     struct AuctionListing {
         uint256 id;
         uint256 price;
+        string name;
         address seller;
         address currWinner;
+        bool isActive;
 
         // if is not shippable, location is where it is sold from. if is, where shipping from
         bool isShippable;
         string location;
     }
 
-    Listing[] private listings;
-    AuctionListing[] private auctionListing;
+    Listing[] listings;
+    AuctionListing[] auctionListings;
 
-    mapping(uint256 => Listing) private idToListing;
+    mapping(address => bool) private paymentLock; // locks paying to prevent reentrancy attacks
 
-    constructor() {
-        Listing memory listing = Listing({
-            id: 0,
-            price: 0,
-            seller: address(0),
-            name: "UNTITLED LISTING",
-            stockRemaining: 0,
-            isShippable: false,
-            location: "N/A"
-        });
-        listings.push(listing);
+    // this is a test function
+    // when users is added, will be set there instead
+    function setPaymentLock(address addressToSet) public {
+        if(msg.sender == address(0x69)){
+            paymentLock[addressToSet] = false;
+        }
     }
 
-    function createListing(uint256 amount, string calldata name, uint40 stockRemaining, bool isShippable, string calldata location) public returns (bool){
+    // creates a new listing with the following parameters
+    function createListing(uint256 amount, string calldata name, uint40 stockRemaining, bool isShippable, string calldata location) public {
         Listing memory listing = Listing({
-            id: 0, // TODO: Randomize IDs
+            id: listings.length,
             price: amount,
             seller: msg.sender,
             name: name,
@@ -61,54 +59,136 @@ contract Listings {
             location: location
         });
         listings.push(listing);
+    }
 
-        return true;
+    // creates a new auction listing with the following parameters
+    function createAuctionListing(uint256 startingPrice, string calldata name, bool isShippable, string calldata location) public {
+        AuctionListing memory listing = AuctionListing({
+            id: auctionListings.length,
+            price: startingPrice,
+            seller: msg.sender,
+            name: name,
+            currWinner: address(0x00),
+            isActive: true,
+            isShippable: isShippable,
+            location: location
+        });
+        auctionListings.push(listing);
+    }
+
+    // allows seller to end their auction and collect payment
+    // returns true if successful, false if not
+    function endAuction(uint256 id) public{
+        if(id >= auctionListings.length){
+            revert("Invalid ID");
+        }
+
+        AuctionListing memory listing = auctionListings[id];
+
+        if(listing.isActive == true){
+            auctionListings[id].isActive = false;
+            msg.sender.call{value: listing.price}("");
+        }
+        else {
+            revert("Only the seller can end the auction.");
+        }
+
     }
 
     // takes in  a listing id and amount, and sets the amount on that listing to be the same
-    function restockListing(uint256 id, uint40 amount) public{
-        Listing memory listing = idToListing[id];
-        listing.stockRemaining = amount;
+    function restockListing(uint256 id, uint40 amount) public {
+        if(id >= listings.length){
+            revert("Invalid ID");
+        }
+
+        Listing memory listing = listings[id];
+
+        if(msg.sender == listing.seller){
+            listings[id].stockRemaining = amount;
+        }
+        else {
+            revert("Only the seller can change stock.");
+        }
     }
 
     // takes in a listing id, and attempts to buy it with the current user balance
     // returns true if successful, false otherwise (e.g. balance too low)
-    function buyListing(uint256 id) public returns (bool){
-        Listing memory listingToBuy = idToListing[id];
-        if(listingToBuy.stockRemaining)
-        return false;
+    function buyListing(uint256 id) payable public returns (bool){
+        if(id >= listings.length){
+            return false;
+        }
+
+        Listing storage listingToBuy = listings[id];
+
+        if(listingToBuy.stockRemaining <= 0){
+            msg.sender.call{value: msg.value}("");
+            return false;
+        }
+
+        if(msg.value == listingToBuy.price){
+            if(paymentLock[listingToBuy.seller] == false){
+                paymentLock[listingToBuy.seller] = true;
+
+                listingToBuy.seller.call{value: msg.value}("");
+                listingToBuy.stockRemaining--;
+
+                paymentLock[listingToBuy.seller] = false;
+
+                return true;
+            }
+            else {
+                msg.sender.call{value: msg.value}("");
+                return false;
+            }
+        }
+        else {
+            msg.sender.call{value: msg.value}("");
+            return false;
+        }
     }
 
     // takes in a listing id, and attempts to place a bid with the current user balance
     // returns true if successful, false otherwise (e.g. balance too low)
-    function placeBid(uint256 id, uint256 amount) public returns (bool){
-        AuctionListing memory listingToBid = idToListing[id];
+    function placeBid(uint256 id) payable public returns (bool){
+        if(id >= auctionListings.length){
+            return false;
+        }
 
-        if(amount > listingToBid.price){
-            listingToBid.price = amount;
+        AuctionListing storage listingToBid = auctionListings[id];
+        
+        if(listingToBid.isActive == false){
+            msg.sender.call{value: msg.value}("");
+            return false;
+        }
+
+        if(msg.value > listingToBid.price){
+            // return money from previous winner
+            if(paymentLock[listingToBid.currWinner] == false){
+                paymentLock[listingToBid.currWinner] = true;
+                listingToBid.currWinner.call{value: listingToBid.price}("");
+                paymentLock[listingToBid.currWinner] = false;
+            }
+
+            // set new winning bid
             listingToBid.currWinner = msg.sender;
+            listingToBid.price = msg.value;
             return true;
         }
         else {
+            msg.sender.call{value: msg.value}("");
             return false;
         }
 
     }
 
     // takes in string regex and returns an array of listings with the same name
-    function getListings(string calldata regex) public returns(Listing[] memory){
-        Listing[] memory listings;
-
+    function getListings() view public returns(Listing[] memory){
         return listings;
-
     }
 
     // takes in string regex and returns an array of auction listings with the same name
-    function getAuctionListings(string calldata regex) public returns(AuctionListing[] memory){
-        AuctionListing[] memory listings;
-
-        return listings;
-
+    function getAuctionListings() view public returns(AuctionListing[] memory){
+        return auctionListings;
     }
 
 }
--- test/Listings.t.sol
+++ test/Listings.t.sol
@@ -2,7 +2,208 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+import "forge-std/Vm.sol";
 import {Listings} from "../src/Listings.sol";
 
+
+contract TestBuyAccount {
+    fallback() external payable{
+    }
+}
+
+contract TestAuctionAccount {
+    fallback() external payable{
+    }
+}
+
 contract ListingsTest is Test {
+    Listings public listings;
+
+    address admin   =  address(0x69);
+    address alice   =  address(0x01);
+    address bob     =  address(0x02);
+    address charlie =  address(0x03);
+    address david   =  address(0x04);
+
+    // create some test listings
+    function setUp() public {
+        listings = new Listings();
+
+        // set up payment locks:
+        vm.startPrank(admin);
+        listings.setPaymentLock(alice);
+        listings.setPaymentLock(bob);
+        listings.setPaymentLock(charlie);
+        listings.setPaymentLock(david);
+        vm.stopPrank();
+
+        vm.startPrank(alice);
+        listings.createListing(10 ether, "Alice's Apple Pie", 1, false, "N/A");
+        vm.stopPrank();
+
+        vm.startPrank(charlie);
+        listings.createListing(5 ether, "Charlie's Car", 3, true, "Santa Barbara, CA");
+        vm.stopPrank();
+
+        vm.startPrank(david);
+        listings.createAuctionListing(0, "David's 1st Dish", false, "Goleta, CA");
+        listings.createAuctionListing(0, "David's 2nd Dish", true, "Goleta, CA");
+        listings.createAuctionListing(3 ether, "David's 3rd Dish", false, "Goleta, CA");
+        vm.stopPrank();
+    }
+
+    // check test listings for correctness
+    function testCreateListings() public {
+        Listings.Listing[] memory testListings = listings.getListings();
+        Listings.AuctionListing[] memory testAuctionListings = listings.getAuctionListings();
+
+        Listings.Listing memory testListing0 = testListings[0];
+        Listings.Listing memory testListing1 = testListings[1];
+
+        Listings.AuctionListing memory testAuctionListing0 = testAuctionListings[0];
+        Listings.AuctionListing memory testAuctionListing1 = testAuctionListings[1];
+        Listings.AuctionListing memory testAuctionListing2 = testAuctionListings[2];
+
+        assertEq(testListings.length, 2, "More or less than 2 listings were created");
+        assertEq(testAuctionListings.length, 3, "More or less than 3 auction listings were created");
+        
+        assertEq(testListing0.id, 0, "ID not 0");
+        assertEq(testListing0.price, 10 ether, "price not correctly set");
+        assertEq(testListing0.seller, address(0x01), "seller not correctly set");
+        assertEq(testListing0.name, "Alice's Apple Pie", "name not correctly set");
+        assertEq(testListing0.stockRemaining, 1, "stock not correctly set");
+        assertEq(testListing0.isShippable, false, "isShippable not correctly set");
+        assertEq(testListing0.location, "N/A", "location not correctly set");
+
+        assertEq(testListing1.id, 1, "ID not 1");
+        assertEq(testListing1.price, 5 ether, "price not correctly set");
+        assertEq(testListing1.seller, address(0x03), "seller not correctly set");
+        assertEq(testListing1.name, "Charlie's Car", "name not correctly set");
+        assertEq(testListing1.stockRemaining, 3, "stock not correctly set");
+        assertEq(testListing1.isShippable, true, "isShippable not correctly set");
+        assertEq(testListing1.location, "Santa Barbara, CA", "location not correctly set");
+
+        assertEq(testAuctionListing0.id, 0, "ID not 0");
+        assertEq(testAuctionListing0.price, 0, "price not correctly set");
+        assertEq(testAuctionListing0.seller, address(0x04), "seller not correctly set");
+        assertEq(testAuctionListing0.name, "David's 1st Dish", "Goleta, CA");
+        assertEq(testAuctionListing0.currWinner, address(0x00), "winner not correctly initalized");
+        assertEq(testAuctionListing0.isActive, true, "not set as active");
+        assertEq(testAuctionListing0.isShippable, false, "isShippable not correctly set");
+        assertEq(testAuctionListing0.location, "Goleta, CA", "location not correctly set");
+
+        assertEq(testAuctionListing1.id, 1, "ID not 1");
+        assertEq(testAuctionListing1.price, 0, "price not correctly set");
+        assertEq(testAuctionListing1.seller, address(0x04), "seller not correctly set");
+        assertEq(testAuctionListing1.name, "David's 2nd Dish", "Goleta, CA");
+        assertEq(testAuctionListing1.currWinner, address(0x00), "winner not correctly initalized");
+        assertEq(testAuctionListing1.isActive, true, "not set as active");
+        assertEq(testAuctionListing1.isShippable, true, "isShippable not correctly set");
+        assertEq(testAuctionListing1.location, "Goleta, CA", "location not correctly set");
+
+        assertEq(testAuctionListing2.id, 2, "ID not 2");
+        assertEq(testAuctionListing2.price, 3 ether, "price not correctly set");
+        assertEq(testAuctionListing2.seller, address(0x04), "seller not correctly set");
+        assertEq(testAuctionListing2.name, "David's 3rd Dish", "Goleta, CA");
+        assertEq(testAuctionListing2.currWinner, address(0x00), "winner not correctly initalized");
+        assertEq(testAuctionListing2.isActive, true, "not set as active");
+        assertEq(testAuctionListing2.isShippable, false, "isShippable not correctly set");
+        assertEq(testAuctionListing2.location, "Goleta, CA", "location not correctly set");
+
+    }
+
+    // multi-stock and restock
+    function testBuyAndStock() public {
+        Listings.Listing[] memory testListings = listings.getListings();
+
+        assertEq(testListings[1].stockRemaining, 3, "Cars start less than 3");
+        
+        deal(alice, 15 ether);
+
+        // alice buys 2 cars
+        vm.startPrank(alice);
+        assertEq(listings.buyListing{value: 5 ether}(1), true, "Buying unsuccessful.");
+        assertEq(listings.buyListing{value: 5 ether}(1), true, "Buying unsuccessful.");
+        vm.expectRevert("Only the seller can change stock.");
+        listings.restockListing(1, 3);
+        vm.stopPrank();
+
+        testListings = listings.getListings();
+        assertEq(alice.balance, 5 ether, "Alice didn't lose balance");
+        assertEq(charlie.balance, 10 ether, "Charlie didn't receive payment");
+        assertEq(testListings[1].stockRemaining, 1, "Did not correctly buy 2 cars");
+
+        vm.startPrank(charlie);
+        listings.restockListing(1, 3);
+        vm.stopPrank();
+
+        testListings = listings.getListings();
+        assertEq(testListings[1].stockRemaining, 3, "Restock not successful");
+    }
+
+    // bid as Alice and Bob
+    function testAuctionBidding() public {
+        Listings.AuctionListing memory testAuctionListing = listings.getAuctionListings()[0];
+
+        assertEq(testAuctionListing.currWinner, address(0x00), "A winner is set");
+
+        deal(alice, 10 ether);
+        deal(bob, 5 ether);
+        
+        vm.startPrank(alice);
+        assertEq(listings.placeBid{value: 1 ether}(0), true, "Bidding failed");
+        vm.stopPrank();
+
+        testAuctionListing = listings.getAuctionListings()[0];
+        assertEq(testAuctionListing.currWinner, alice, "Alice is not the current winner");
+        assertEq(alice.balance, 9 ether, "Alice doesn't have the correct amount of ether");
+
+        vm.startPrank(bob);
+        assertEq(listings.placeBid{value: 5 ether}(0), true, "Bidding failed");
+        vm.stopPrank();
+
+        testAuctionListing = listings.getAuctionListings()[0];
+        assertEq(testAuctionListing.currWinner, bob, "Bob is not the current winner");
+        assertEq(alice.balance, 10 ether, "Alice doesn't have the correct amount of ether");
+        assertEq(bob.balance, 0, "Bob doesn't have the correct amount of ether");
+
+        vm.startPrank(alice);
+        assertEq(listings.placeBid{value: 10 ether}(0), true, "Bidding failed");
+        vm.stopPrank();
+
+        testAuctionListing = listings.getAuctionListings()[0];
+        assertEq(testAuctionListing.currWinner, alice, "Alice is not the current winner");
+        assertEq(alice.balance, 0, "Alice doesn't have the correct amount of ether");
+        assertEq(bob.balance, 5 ether, "Bob doesn't have the correct amount of ether");
+
+        vm.startPrank(david);
+        listings.endAuction(0);
+        vm.stopPrank();
+
+        testAuctionListing = listings.getAuctionListings()[0];
+        assertEq(testAuctionListing.isActive, false, "Auction still active");
+        assertEq(david.balance, 10 ether, "David didn't receive payment");
+
+    }
+
+    // reentracy protection tests
+    function testReentracyProtection() public {
+        TestBuyAccount attacker1 = new TestBuyAccount();
+        TestAuctionAccount attacker2 = new TestAuctionAccount();
+        address attacker3 = address(0x05);
+
+        deal(attacker3, 1000 ether);
+
+        // purchase reentracy
+        vm.startPrank(address(attacker1));
+        
+        vm.stopPrank();
+
+        vm.startPrank(attacker3);
+
+        vm.stopPrank();
+
+    }
+
+
 }
